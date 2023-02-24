c50313e3be09211977b618605bb7aa926e736ff5
VikaVelhan <106451622+VikaVelhan@users.noreply.github.com>
2023-02-24 21:01:26 +0000

    Deploying to gh-pages from @ VikaVelhan/goit-react-hw-05-movies@761e38d448ef5013818a115bd0ba946f3db70430 🚀

---
+++ static/js/805.9067f512.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Page not found"})})}}}]);
+//# sourceMappingURL=805.9067f512.chunk.js.map
